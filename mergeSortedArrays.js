47335199df4b26416f8fdc3247ae29b4e9098e2a
akhtarzaheen <akhtarzaheen335@gmail.com>
2026-08-30 21:24:04 +0530

    solved moergedSorted with second aproach

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -3,22 +3,36 @@ var merge = function(nums1, m, nums2, n) {
     let k=0;
     let i=0;
     let j=0;
-    while(k<m){
+    let largeLength = m>n ? m : n
+    while(k<n+m){
         if(nums1[i]<=nums2[j]){
+            result.push(nums1[i])
             i++;
         }else{
-            nums1[i+1] = nums1[i];
-            nums1[i] = nums2[j];
+            result.push(nums2[j]);
+            j++;
         }
-
         k++;
     }
+    console.log('i,j=>',i,j)
+    if(i<m){
+        while(i<m){
+                result.push(nums1[i])
+                i++;
+        }
+    }
+    if(j<n){
+        console.log
+        while(j<n){
+                result.push(nums2[j]);
+                j++;
+        }
+    }
+    return result;
 };
 let nums1 = [1,2,3];
-let m = 6;
-let nums2 = [2,5,6];
-let n = 3;
+let m = 3;
+let nums2 = [2,5,6,9];
+let n = 4;
 
-merge(nums1, m, nums2, n)
-
-console.log('res=>',nums1)
+console.log('res=>',merge(nums1, m, nums2, n))
