52b7a1a81ac9d4d93d720863f80ccfc11c5a5305
akhtarzaheen <akhtarzaheen335@gmail.com>
2026-08-30 22:00:38 +0530

    bugfixes

--- mergeSortedArrays.js
+++ mergeSortedArrays.js
@@ -1,4 +1,6 @@
-var merge = function(nums1, m, nums2, n) {
+// Aproach 1
+
+var merge1 = function(nums1, m, nums2, n) {
     let result  = [];
     let k=0;
     let i=0;
@@ -28,9 +30,31 @@ var merge = function(nums1, m, nums2, n) {
     }
     return result;
 };
+
+// Apprach 2
+
+var merge2 = function(nums1, m, nums2, n) {
+    let nums1Copy = nums1.slice(0,m);
+    console.log('nums1Copy=>',nums1Copy)
+    let k=0;
+    let i=0;
+    let j=0;
+    while(k<n+m){
+        if((nums1Copy[i]<nums2[j] && i<m) || j>=n){
+            nums1[k] = nums1Copy[i];
+            i++;
+        }else{
+            nums1[k] = nums2[j];
+            j++;
+        }
+        k++;
+    }
+};
 let nums1 = [1,2,3];
 let m = 3;
 let nums2 = [2,5,6,9];
 let n = 4;
 
-console.log('res=>',merge(nums1, m, nums2, n))
+merge2(nums1, m, nums2, n)
+
+console.log('nums1=>',nums1)
